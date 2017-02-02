3651716e84e663f1da1edb3a9164b24a972a8408
Katie0001 <katie@altvr.com>
2017-02-02 12:07:47 -0800

    fixed formatting

--- questions.js
+++ questions.js
@@ -14,27 +14,27 @@
         "What sport or activity did you do in school?",
         "How do you want to be remembered?",
         "What do you spend most of your money on?",
-        "Would you rather live in the city or the woods?"
-        "What's your favorite comedy?"
+        "Would you rather live in the city or the woods?","
+        "What's your favorite comedy?","
         "If you could travel anywhere in the world, where would it be?",
         "If you could live forever, would you? How would you spend eternity?"
         "What message would you choose to send to an advanced alien species?",
         "What three emoji's describe you best?",
         "What fad do you wish would come back? Which do you feel should stay gone forever?",
         "What's your favorite dish to cook?",
-        "If it were possible have a pet dinosaur, what dinosaur would you want?
-        "What's the best concert you've ever attended?"
+        "If it were possible have a pet dinosaur, what dinosaur would you want?",
+        "What's the best concert you've ever attended?",
         "What dream have you had that still resontes with you?",
-        "Which are you more likely to fight for, love or money? Why?
+        "Which are you more likely to fight for, love or money? Why?",
         "If you could change one law, what would it be?",
         "Would you rather explore the bottom of the ocean or a new planet?",
         "What's the most traumatic movie or book you saw/read as a child?",
         "Describe your family in a nutshell.",
         "If you could bring back one fictional character to life, who would it be and why?",
-        "If you could time travel, would you go to the past or future?"
+        "If you could time travel, would you go to the past or future?",
         "What would be your perfect day?",
         "What is your favorite childhood book?",
-        "If you could choose your last words, what would they be?"
+        "If you could choose your last words, what would they be?",
         "If you had a theme song what would it be?",
         "If you could change one event from history, what would it be?",
         "What unusual skills do you have?",
@@ -59,7 +59,7 @@
         "What's your favorite holiday?",
         "Are you spiritual, religious, agnostic, athiest? What does spirituality mean to you?",
         "What show are you binge watching now?",
-        "Who’s your personal hero?"
+        "Who’s your personal hero?",
         "What is one of the most spontaneous things you've ever done?",
         "How did your parents meet?",
         "What did you want to be when you grew up?",
@@ -68,7 +68,7 @@
         "What was the last movie you went to? What did you think?",
         "Do you know any good ghost stories?",
         "Do you like or dislike surprises? Why or why not?",
-        "What was your closest near-death experience?
+        "What was your closest near-death experience?",
         "What book or TV show changed your life?",
         "Is there something that you've dreamt of doing for a long time? Why haven't you done it?",
         "What was your first job?",
